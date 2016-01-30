66481df09b8253d5ab1ee9829c4266fb8962916c
Zhannator <zhp3@pitt.edu>
2016-01-30 14:45:37 -0500

    First Changes

---
+++ app/src/main/java/tourmates/pictouresque/Activity2.java
@@ -0,0 +1,15 @@
+package tourmates.pictouresque;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Activity2 extends Activity {
+    //supposed to receive a picture from MainActivity
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+
+}
